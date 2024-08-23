b6f7c052c126789e9f685f0f0cec3c097b1a739a
Priyang1310 <139229134+Priyang1310@users.noreply.github.com>
2024-08-23 22:26:40 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             domain="dev-dh54w48gok42komr.us.auth0.com"
             clientId="AlJmSXRtopg3H7gBzrxC0B9pmOuqWI5A"
             authorizationParams={{
-              redirect_uri: 'https://chat-with-database.vercel.app/',
+              redirect_uri: 'https://chat-with-database.vercel.app/chatbot',
             }}
           >
             <App />
